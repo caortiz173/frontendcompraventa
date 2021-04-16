ae3965c1d28715f55515834c6c124242491f8956
caortiz173 <camiloandresortizrincon@gmail.com>
2021-04-16 14:34:41 -0500

    proyecto frontend

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,32 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (login.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '../components/login.vue')
+  },
+  {
+    path: '/prueba',
+    name: 'prueba',
+    // route level code-splitting
+    // this generates a separate chunk (login.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '../components/prueba.vue')
+  },  
+  {
+    path: '/menu',
+    name: 'menu',
+    // route level code-splitting
+    // this generates a separate chunk (login.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "menu" */ '../components/menu.vue')
   }
+
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <roles/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import roles from '@/views/roles.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    roles
+    
   }
 }
 </script>
